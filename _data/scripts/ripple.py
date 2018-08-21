6fbde6b582b458c9a05c7624d609dd1c1a0defa6
Daniel Goldman <dzgoldman@wesleyan.edu>
2018-08-21 10:09:32 -0400

    use ledger.exposed for wealth distribution stats

--- _data/scripts/ripple.py
+++ _data/scripts/ripple.py
@@ -16,7 +16,7 @@ class Ripple(CoinScrapper):
             print('XRP public nodes', public_nodes_count)
             return public_nodes_count
         except:
-            print('XRP api failed, falling back to web crawler:')
+            print('XRP api failed (xrp charts), falling back to web crawler:')
             self.get_page("https://xrpcharts.ripple.com/#/topology", sleep_time = 4)
             nodes_element = self.attempt_find_element( lambda: self.driver.find_element_by_class_name('nNodes'))
             public_nodes_count =  int(nodes_element.text)
@@ -24,10 +24,31 @@ class Ripple(CoinScrapper):
             return public_nodes_count
         
     def get_wealth_distribution(self):
-        self.get_page("https://ledger.exposed/rich-stats");
-        wealth_distribution =  self.attempt_find_element( lambda:self.driver.find_element_by_css_selector('[data-v-ca88cbc2].large > b')).text
-        wealth_distribution = self.percentage_string_to_float(wealth_distribution)
-        return wealth_distribution
+        try:
+            # get sum of top 100 accounts
+            res = requests.get('https://ledger.exposed/api/wallet-toplist/100/0')
+            accounts = res.json()
+            balance_sum = sum([account['Balance'] for account in accounts])
+
+            # get total non-escrowed coin count:
+            res = requests.get('https://ledger.exposed/api/richlist')
+            data = res.json()['has']
+            total_coins = sum([data[key]['balanceSum'] for key in data])
+            wealth_distribution = balance_sum / total_coins
+            assert(isinstance(wealth_distribution, float))
+            
+            # convert to %
+            wealth_distribution = round(wealth_distribution * 100)
+            print('XRP wealth distribution', wealth_distribution)
+            return wealth_distribution
+            
+        except:   
+            print('XRP api failed (ledger exposed), falling back to web crawler:') 
+            # in case api is down but site is still up? unlikely, but may as well try:
+            self.get_page("https://ledger.exposed/rich-stats");
+            wealth_distribution =  self.attempt_find_element( lambda:self.driver.find_element_by_css_selector('[data-v-ca88cbc2].large > b')).text
+            wealth_distribution = self.percentage_string_to_float(wealth_distribution)
+            return wealth_distribution
     def get_client_codebases(self):
         # A bit hard to get so leaving hard-coded for now; I suspect it won't be an issue any time soon ;)
         return 1
