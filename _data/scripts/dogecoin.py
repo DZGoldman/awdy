05b671870e17b9d31e5a2d9d49c582ec858b3783
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-02-15 16:10:15 -0500

    such webcrawling, wow

--- _data/scripts/dogecoin.py
+++ _data/scripts/dogecoin.py
@@ -1,4 +1,5 @@
 from coinscrapper import CoinScrapper
+import requests 
 
 class Dogecoin(CoinScrapper):
 
@@ -7,14 +8,22 @@ class Dogecoin(CoinScrapper):
         self.driver = driver
 
     def get_public_nodes(self):
-        return 'n/a'
+        self.get_page("https://www.coinexchange.io/network/peers/DOGE")
+        els = self.find_elements('h4.animated-title')
+        node_els = [el for el in els if "Active Peers" in el.text]
+        assert(len(node_els) == 1)
+        count = self.extract_first_int(node_els[0].text)
+        return count
         
     def get_wealth_distribution(self):
         return self.bitinfo_wealth_dist()
 
     def get_client_codebases(self):
-        return 'n/a'
+        return 1
 
     def get_consensus_distribution(self):
         # TODO can't get from source (pie chart)
-        pass
+        res = requests.get('https://chain.so/api/v2/block/DOGE/miners')
+        data = res.json()['data']
+        percent_data = [data[key] for key in data]
+        return self.get_cumulative_grouping_count(percent_data, 0.5)
