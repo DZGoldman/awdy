d5d3ad76a32eed899eb92739a62c8d3377878405
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-02-15 13:43:25 -0500

    add zcash mining, reactivate

--- _data/scripts/zcash.py
+++ _data/scripts/zcash.py
@@ -5,6 +5,7 @@ class ZCash(CoinScrapper):
     def __init__ (self, driver):
         self.name = 'zcash'
         self.driver = driver
+        
 
     def get_public_nodes(self):
         self.get_page("https://explorer.zcha.in/network")
@@ -20,4 +21,4 @@ class ZCash(CoinScrapper):
 
     def get_consensus_distribution(self):
         # TODO can't get from source (pie chart)
-        pass
+        return self.pool_stats_consensus_scrape()
