12d49751c5d8b6b9021fe00b93f8e782ef2cb6c8
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-03-10 12:29:05 +0800

    add nano

--- _data/scripts/nano.py
+++ _data/scripts/nano.py
@@ -1,31 +1,27 @@
 from coinscrapper import CoinScrapper
+import requests
+
 class Nano(CoinScrapper):
 
     def __init__ (self, driver):
         self.name = 'nano'
         self.driver = driver
-    # NOTE: these may be a bit too ad hoc
+
     def get_public_nodes(self):
-        self.get_page('https://nanocrawler.cc/status')
-        els = self.find_elements('.text-muted')
-        peer_el = [el for el in els if el.text == 'Peers'][0]
-        parent = peer_el.find_element_by_xpath('..')  
-        return self.extract_first_int(parent.text)
+        res = requests.get('https://api.nanocrawler.cc/peer_count')
+        return res.json()['peerCount']
+
 
     def get_wealth_distribution(self):
-        # source down:
-        self.get_page('https://nanocrawler.cc/explorer/top_accounts')
-        els = self.find_elements('.text-dark')
-        el = [el for el in els if "total supply" in el.text][0]
-        return self.extract_first_int(el.text)
+        data = requests.get('https://api.nanocrawler.cc/accounts/1').json()['accounts'] + requests.get('https://api.nanocrawler.cc/accounts/2').json()['accounts']
+        assert(len(data)== 100)
+        total =  sum( [float(d['balance']) for d in data] )
+        return 100*total/133248289
 
     def get_client_codebases(self):
         return 1
 
     def get_consensus_distribution(self):
-        self.get_page('https://mynano.ninja/active')
-        # Note: this may be too vague
-        data = self.find_elements('.h1')
-        percentages = [self.percentage_string_to_float(el.text) for el in data]
-        return self.get_cumulative_grouping_count(percentages, .5)
+        res = requests.get('https://repnode.org/api/adjusted-nakamoto')
+        return res.json()['adjusted_nakamoto_online']
 
